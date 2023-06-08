a313507a14656e0a29a35437e94b64879c346588
sebasesco <sebastian.03escobar@gmail.com>
2023-06-08 00:52:50 -0400

    Added about us and contact pages. Minor css

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,17 @@
 import React from 'react'
+import '../styles/Home.css'
 
 export const Home = () => {
     return (
         <div>
-            This is the home page
+            <div className='intro'>
+                Ready to find your dream location? SmartSettle has you covered! Whether you're seeking a vibrant city, a peaceful suburban retreat, or a coastal paradise, SmartSettle is your ultimate companion in the search for the perfect place to live. With advanced algorithms and comprehensive data analysis, SmartSettle takes into account your unique preferences, priorities, and lifestyle requirements to deliver personalized recommendations that match your vision of an ideal home.
+            </div>
+            <div className='signup'>
+                Get Started!
+                Don't have an account?
+                Sign up! 
+            </div>
         </div>
     )
 }
