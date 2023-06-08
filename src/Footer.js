5a4d0bf37dccb3e88a493a0e615bae7a5fd869e0
sebasesco <sebastian.03escobar@gmail.com>
2023-06-08 01:20:43 -0400

    Messed with some css

--- src/Footer.js
+++ src/Footer.js
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom"
 
 export const Footer = () => {
     return (
-        <div className='navbar'>
+        <div className='footer'>
             <Link to="/about"> About Us </Link>
             <Link to="/contact"> Contact </Link>
         </div>
