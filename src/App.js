43d9a049a77a69b14c046226229ea6978ae48937
sebasesco <sebastian.03escobar@gmail.com>
2023-06-07 23:57:22 -0400

    Added routes and basic navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import './styles/App.css';
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom"
+import {Home} from "./pages/Home"
+import {Login} from "./pages/Login"
+import {Navbar} from "./Navbar"
 
 function App() {
   return (
     <div className="App">
-      bs
+      <Router>
+        <Navbar></Navbar>
+        <Routes>
+          <Route path='/' element={<Home/>} />
+          <Route path='/login' element={<Login/>} />
+        </Routes>
+      </Router>
     </div>
   );
 }
