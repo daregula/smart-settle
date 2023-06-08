06b7708ffa81481ff134375f447d5039750148be
sebasesco <sebastian.03escobar@gmail.com>
2023-06-08 00:30:14 -0400

    added some navbar css

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import './styles/Navbar.css';
 import { Link } from "react-router-dom"
 
 export const Navbar = () => {
   return (
-    <div>
+    <div className='navbar'>
         <Link to="/"> Home </Link>
         <Link to="/login"> Login </Link>
     </div>
