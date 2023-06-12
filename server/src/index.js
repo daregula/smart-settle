24eec5e4d1a21973cd8c3c3e1b213b170dab7066
verdecit <verdecit@kean.edu>
2023-06-11 23:52:31 -0400

    connecting db and adding login/register functions

--- server/src/index.js
+++ server/src/index.js
@@ -1,10 +1,17 @@
-import express from "express";
-import cors from "cors";
+import express from "express"
+import cors from "cors"
 import mongoose from "mongoose"
+import { userRouter } from "./routes/users.js"
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
+
+app.use("/auth", userRouter)
+
+// create an environment variable for the mongodb authentication
+mongoose.connect("mongodb+srv://webapp:6XPWFVBAbSAXiNpx@smart-settle.hvwbigu.mongodb.net/ssdb?retryWrites=true&w=majority")
+
 
 app.listen(3001, () => console.log("Server Started"));
---
+++ server/src/routes/users.js
@@ -0,0 +1,52 @@
+import express from "express"
+import jwt from "jsonwebtoken"
+import bcrypt from "bcrypt"
+import { UserModel } from "../models/Users.js"
+
+const router = express.Router()
+// creating the two route endpoints for registering a user
+router.post("/register", async (req, res) => {
+    const { username, password } = req.body
+
+    // creating a request to our mongodb collections
+    const user = await UserModel.findOne({ username })
+
+    if (user) {
+        return res.json({ message: "User already exists" })
+    }
+    
+    const hashedPassword = await bcrypt.hash(password, 10)
+
+    const newUser = new UserModel({ username, password: hashedPassword })
+    await newUser.save()
+
+    res.json({ message:"User registered successfully" })
+})
+
+// creating the route for user login
+router.post("/login", async (req, res) => {
+    // creating variables that will hold the username and passwords that the user will be providing
+    const { username, password } = req.body
+
+    // fetching the user entry from the database
+    const user = await UserModel.findOne({ username })
+
+    // if user returns a null value we return this error message
+    if (!user) {
+        return res.json({ message: "User does not exist "})
+    }
+
+    // bycrypt built in function that we use to compare the password provided by the user with the password that we have in the database for that username
+    const isPasswordValid = await bcrypt.compare(password, user.password)
+
+    // if password is incorrect we will be returned a false value and in turn return an error message
+    if (!isPasswordValid) {
+        return res.json({ message: "Username or Password is Incorrect" })
+    }
+    // create an environment variable for the string secret
+    const token = jwt.sign({ id: user._id }, "secret")
+    res.json({ token, userID: user._id })
+
+})
+
+export { router as userRouter }
