79c7a0bef946612eaad227957d70cd99f06e1252
sebasesco <sebastian.03escobar@gmail.com>
2023-06-14 23:03:10 -0400

    bs

--- client/src/pages/Survey.js
+++ client/src/pages/Survey.js
@@ -4,22 +4,27 @@ import { useGetUserID } from '../hooks/useGetUserID';
 import { useNavigate } from 'react-router-dom';
 import '../styles/Survey.css'
 import {
-    Flex,
+    Progress,
     Box,
-    Select,
-    FormControl,
-    FormLabel,
-    Input,
-    InputGroup,
-    HStack,
-    InputRightElement,
-    Stack,
+    ButtonGroup,
     Button,
     Heading,
-    Text,
-    useColorModeValue,
-    Link,
-} from '@chakra-ui/react';
+    Flex,
+    FormControl,
+    GridItem,
+    FormLabel,
+    Input,
+    Select,
+    SimpleGrid,
+    InputLeftAddon,
+    InputGroup,
+    Textarea,
+    FormHelperText,
+    InputRightElement,
+  } from '@chakra-ui/react';
+  
+  import { useToast } from '@chakra-ui/react';
+
 
 export const Survey = () => {
     const userID = useGetUserID();
@@ -57,49 +62,16 @@ export const Survey = () => {
     return (
         
         <div className='survey'>
+            
+            <h1>Survey</h1>
             <form id="form" onSubmit={onSubmit}>
-            <Flex
-                minH={'75vh'}
-                align={'center'}
-                justify={'center'}
-                bg={useColorModeValue('gray.50', 'gray.800')}>
-                <Stack spacing={8} mx={'auto'} maxW={'xlg'} py={12} px={6}>
-                <Stack align={'center'}>
-                    <Heading fontSize={'4xl'} textAlign={'center'}>
-                    Survey
-                    </Heading>
-                </Stack>
-                <Box
-                rounded={'lg'}
-                bg={useColorModeValue('white', 'gray.700')}
-                boxShadow={'lg'}
-                p={8}>
-                <HStack>
-                    <div class="form-control">
-                    <FormLabel>Salary</FormLabel>
-                    <Input type='text' id='salary' name='salary' onChange={handleChange} />
+                <div class="form-control">
+                    <label htmlFor='salary'> Salary </label>
+                    <input type='text' id='salary' name='salary' onChange={handleChange}></input>
                 </div>
                 <div class="form-control">
-                <FormLabel>Employment</FormLabel>
-                    <Select type='text' id='employment' name='employment' onChange={handleChange}>
-                        <option value="1">industry1</option>
-                        <option value="2">industry2</option>
-                        <option value="3">industry3</option>
-                        <option value="4">industry4</option>
-                        <option value="5">industry5</option>
-                        <option value="6">industry6</option>
-                        <option value="7">industry7</option>
-                        <option value="8">industry8</option>
-                        <option value="9">industry9</option>
-                        <option value="10">industry10</option>
-                    </Select>
-                </div>
-                </HStack>
-                
-                <HStack>
-                <div class="form-control">
-                    <FormLabel>Education</FormLabel>
-                    <Select type='text' id='education' name='education' onChange={handleChange}>
+                    <label htmlFor='employment'> Employment </label>
+                    <select type='text' id='employment' name='employment' onChange={handleChange}>
                         <option value="1">1</option>
                         <option value="2">2</option>
                         <option value="3">3</option>
@@ -110,12 +82,28 @@ export const Survey = () => {
                         <option value="8">8</option>
                         <option value="9">9</option>
                         <option value="10">10</option>
-                    </Select>
+                    </select>
+                </div>
+                
+                <div class="form-control">
+                    <label htmlFor='education'> Education </label>
+                    <select type='text' id='education' name='education' onChange={handleChange}>
+                        <option value="1">1</option>
+                        <option value="2">2</option>
+                        <option value="3">3</option>
+                        <option value="4">4</option>
+                        <option value="5">5</option>
+                        <option value="6">6</option>
+                        <option value="7">7</option>
+                        <option value="8">8</option>
+                        <option value="9">9</option>
+                        <option value="10">10</option>
+                    </select>
                 </div>
 
                 <div class="form-control">
-                    <FormLabel>Weather</FormLabel>   
-                    <Select type='text' id='weather' name='weather' onChange={handleChange}>
+                    <label htmlFor='weather'> Weather </label>
+                    <select type='text' id='weather' name='weather' onChange={handleChange}>
                         <option value="1">1</option>
                         <option value="2">2</option>
                         <option value="3">3</option>
@@ -126,12 +114,12 @@ export const Survey = () => {
                         <option value="8">8</option>
                         <option value="9">9</option>
                         <option value="10">10</option>
-                    </Select>
+                    </select>
                 </div>
-                </HStack>
+
                 <div class="form-control">
-                    <FormLabel>Transportation</FormLabel>
-                    <Select type='text' id='transportation' name='transportation' onChange={handleChange}>
+                    <label htmlFor='transportation'> Transportation </label>
+                    <select type='text' id='transportation' name='transportation' onChange={handleChange}>
                         <option value="1">1</option>
                         <option value="2">2</option>
                         <option value="3">3</option>
@@ -142,24 +130,340 @@ export const Survey = () => {
                         <option value="8">8</option>
                         <option value="9">9</option>
                         <option value="10">10</option>
-                    </Select>
+                    </select>
                 </div>
-                <Button
-                type="submit"
-                loadingText="Submitting"
-                size="lg"
-                bg={'purple.400'}
-                color={'white'}
-                _hover={{
-                    bg: 'purple.300',
-                }}>
-                Submit
-                </Button>
-                </Box>
-                </Stack>
-                </Flex>
+                    <button type='submit'> Submit </button>
             </form>
             
         </div>
     )
 }
+
+  
+  const Form1 = () => {
+    const [show, setShow] = React.useState(false);
+    const handleClick = () => setShow(!show);
+    return (
+      <>
+        <Heading w="100%" textAlign={'center'} fontWeight="normal" mb="2%">
+          User Registration
+        </Heading>
+        <Flex>
+          <FormControl mr="5%">
+            <FormLabel htmlFor="first-name" fontWeight={'normal'}>
+              First name
+            </FormLabel>
+            <Input id="first-name" placeholder="First name" />
+          </FormControl>
+  
+          <FormControl>
+            <FormLabel htmlFor="last-name" fontWeight={'normal'}>
+              Last name
+            </FormLabel>
+            <Input id="last-name" placeholder="First name" />
+          </FormControl>
+        </Flex>
+        <FormControl mt="2%">
+          <FormLabel htmlFor="email" fontWeight={'normal'}>
+            Email address
+          </FormLabel>
+          <Input id="email" type="email" />
+          <FormHelperText>We'll never share your email.</FormHelperText>
+        </FormControl>
+  
+        <FormControl>
+          <FormLabel htmlFor="password" fontWeight={'normal'} mt="2%">
+            Password
+          </FormLabel>
+          <InputGroup size="md">
+            <Input
+              pr="4.5rem"
+              type={show ? 'text' : 'password'}
+              placeholder="Enter password"
+            />
+            <InputRightElement width="4.5rem">
+              <Button h="1.75rem" size="sm" onClick={handleClick}>
+                {show ? 'Hide' : 'Show'}
+              </Button>
+            </InputRightElement>
+          </InputGroup>
+        </FormControl>
+      </>
+    );
+  };
+  
+  const Form2 = () => {
+    return (
+      <>
+        <Heading w="100%" textAlign={'center'} fontWeight="normal" mb="2%">
+          User Details
+        </Heading>
+        <FormControl as={GridItem} colSpan={[6, 3]}>
+          <FormLabel
+            htmlFor="country"
+            fontSize="sm"
+            fontWeight="md"
+            color="gray.700"
+            _dark={{
+              color: 'gray.50',
+            }}>
+            Country / Region
+          </FormLabel>
+          <Select
+            id="country"
+            name="country"
+            autoComplete="country"
+            placeholder="Select option"
+            focusBorderColor="brand.400"
+            shadow="sm"
+            size="sm"
+            w="full"
+            rounded="md">
+            <option>United States</option>
+            <option>Canada</option>
+            <option>Mexico</option>
+          </Select>
+        </FormControl>
+  
+        <FormControl as={GridItem} colSpan={6}>
+          <FormLabel
+            htmlFor="street_address"
+            fontSize="sm"
+            fontWeight="md"
+            color="gray.700"
+            _dark={{
+              color: 'gray.50',
+            }}
+            mt="2%">
+            Street address
+          </FormLabel>
+          <Input
+            type="text"
+            name="street_address"
+            id="street_address"
+            autoComplete="street-address"
+            focusBorderColor="brand.400"
+            shadow="sm"
+            size="sm"
+            w="full"
+            rounded="md"
+          />
+        </FormControl>
+  
+        <FormControl as={GridItem} colSpan={[6, 6, null, 2]}>
+          <FormLabel
+            htmlFor="city"
+            fontSize="sm"
+            fontWeight="md"
+            color="gray.700"
+            _dark={{
+              color: 'gray.50',
+            }}
+            mt="2%">
+            City
+          </FormLabel>
+          <Input
+            type="text"
+            name="city"
+            id="city"
+            autoComplete="city"
+            focusBorderColor="brand.400"
+            shadow="sm"
+            size="sm"
+            w="full"
+            rounded="md"
+          />
+        </FormControl>
+  
+        <FormControl as={GridItem} colSpan={[6, 3, null, 2]}>
+          <FormLabel
+            htmlFor="state"
+            fontSize="sm"
+            fontWeight="md"
+            color="gray.700"
+            _dark={{
+              color: 'gray.50',
+            }}
+            mt="2%">
+            State / Province
+          </FormLabel>
+          <Input
+            type="text"
+            name="state"
+            id="state"
+            autoComplete="state"
+            focusBorderColor="brand.400"
+            shadow="sm"
+            size="sm"
+            w="full"
+            rounded="md"
+          />
+        </FormControl>
+  
+        <FormControl as={GridItem} colSpan={[6, 3, null, 2]}>
+          <FormLabel
+            htmlFor="postal_code"
+            fontSize="sm"
+            fontWeight="md"
+            color="gray.700"
+            _dark={{
+              color: 'gray.50',
+            }}
+            mt="2%">
+            ZIP / Postal
+          </FormLabel>
+          <Input
+            type="text"
+            name="postal_code"
+            id="postal_code"
+            autoComplete="postal-code"
+            focusBorderColor="brand.400"
+            shadow="sm"
+            size="sm"
+            w="full"
+            rounded="md"
+          />
+        </FormControl>
+      </>
+    );
+  };
+  
+  const Form3 = () => {
+    return (
+      <>
+        <Heading w="100%" textAlign={'center'} fontWeight="normal">
+          Social Handles
+        </Heading>
+        <SimpleGrid columns={1} spacing={6}>
+          <FormControl as={GridItem} colSpan={[3, 2]}>
+            <FormLabel
+              fontSize="sm"
+              fontWeight="md"
+              color="gray.700"
+              _dark={{
+                color: 'gray.50',
+              }}>
+              Website
+            </FormLabel>
+            <InputGroup size="sm">
+              <InputLeftAddon
+                bg="gray.50"
+                _dark={{
+                  bg: 'gray.800',
+                }}
+                color="gray.500"
+                rounded="md">
+                http://
+              </InputLeftAddon>
+              <Input
+                type="tel"
+                placeholder="www.example.com"
+                focusBorderColor="brand.400"
+                rounded="md"
+              />
+            </InputGroup>
+          </FormControl>
+  
+          <FormControl id="email" mt={1}>
+            <FormLabel
+              fontSize="sm"
+              fontWeight="md"
+              color="gray.700"
+              _dark={{
+                color: 'gray.50',
+              }}>
+              About
+            </FormLabel>
+            <Textarea
+              placeholder="you@example.com"
+              rows={3}
+              shadow="sm"
+              focusBorderColor="brand.400"
+              fontSize={{
+                sm: 'sm',
+              }}
+            />
+            <FormHelperText>
+              Brief description for your profile. URLs are hyperlinked.
+            </FormHelperText>
+          </FormControl>
+        </SimpleGrid>
+      </>
+    );
+  };
+  
+  export default function Multistep() {
+    const toast = useToast();
+    const [step, setStep] = useState(1);
+    const [progress, setProgress] = useState(33.33);
+    return (
+      <>
+        <Box
+          borderWidth="1px"
+          rounded="lg"
+          shadow="1px 1px 3px rgba(0,0,0,0.3)"
+          maxWidth={800}
+          p={6}
+          m="10px auto"
+          as="form">
+          <Progress
+            hasStripe
+            value={progress}
+            mb="5%"
+            mx="5%"
+            isAnimated></Progress>
+          {step === 1 ? <Form1 /> : step === 2 ? <Form2 /> : <Form3 />}
+          <ButtonGroup mt="5%" w="100%">
+            <Flex w="100%" justifyContent="space-between">
+              <Flex>
+                <Button
+                  onClick={() => {
+                    setStep(step - 1);
+                    setProgress(progress - 33.33);
+                  }}
+                  isDisabled={step === 1}
+                  colorScheme="teal"
+                  variant="solid"
+                  w="7rem"
+                  mr="5%">
+                  Back
+                </Button>
+                <Button
+                  w="7rem"
+                  isDisabled={step === 3}
+                  onClick={() => {
+                    setStep(step + 1);
+                    if (step === 3) {
+                      setProgress(100);
+                    } else {
+                      setProgress(progress + 33.33);
+                    }
+                  }}
+                  colorScheme="teal"
+                  variant="outline">
+                  Next
+                </Button>
+              </Flex>
+              {step === 3 ? (
+                <Button
+                  w="7rem"
+                  colorScheme="red"
+                  variant="solid"
+                  onClick={() => {
+                    toast({
+                      title: 'Account created.',
+                      description: "We've created your account for you.",
+                      status: 'success',
+                      duration: 3000,
+                      isClosable: true,
+                    });
+                  }}>
+                  Submit
+                </Button>
+              ) : null}
+            </Flex>
+          </ButtonGroup>
+        </Box>
+      </>
+    );
+  }
