abfc24e2ef699019c471561e579dcdeec902bc1e
sebasesco <sebastian.03escobar@gmail.com>
2023-06-21 20:56:03 -0400

    bs

--- server/src/routes/result.js
+++ server/src/routes/result.js
@@ -1,5 +1,5 @@
 import  express  from "express";
-
+import fs from "fs";
 const router = express.Router();
 
 // Created an API endpoint to recieve survey response data
@@ -9,7 +9,28 @@ router.post("/", async (req, res) => {
 
     //We need to implement some algorithm using the response data to return some result
     console.log(data);
-    
+    fs.readFile("../server/src/sample-data/cost_of_living.json", "utf8", (err, res) => {
+        if (err) {
+            console.log("File read failed:", err);
+            return;
+        }
+        const resultData = JSON.parse(res)
+        const monthlyIncome = (parseInt(data.salary) * 0.3) / 12;
+        const filteredCOL = []
+        for (const entry of resultData) {
+            const { state, city, cost_of_living } = entry;
+            if(monthlyIncome > cost_of_living) {
+                filteredCOL.push(entry);
+            }
+        }
+        console.log(filteredCOL);
+        
+    });
 })
+
+
+
+
+
 
 export { router as resultRouter }
