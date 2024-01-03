4d104110782583b6656028c0f017283b639d0fb0
verdecit <tedverdecia@gmail.com>
2024-01-02 21:36:42 -0500

    are

--- server/src/index.js
+++ server/src/index.js
@@ -15,7 +15,7 @@ const app = express();
 app.use(express.json());
 app.use(cors(
     {
-        origin: [""],
+        origin: ["https://smart-settle-frontend.vercel.app/"],
         method: ["POST", "GET"],
         credentials: true
     }
