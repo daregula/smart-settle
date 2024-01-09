d501567d15569cc51ca3399fad9a618195eedad4
verdecit <tedverdecia@gmail.com>
2024-01-09 17:16:13 -0500

    trying vercel solution again

--- server/src/routes/result.js
+++ server/src/routes/result.js
@@ -44,9 +44,8 @@ router.post("/", async (req, res) => {
         console.log("output from ls->\n",stdout);
     });
 
-    // const file = path.join(process.cwd(), 'server','public','assets', 'cost_of_living_cp.json');
-    // const stringified = readFileSync(file, 'utf8');
-    // console.log("vercel docs solution:\n",stringified);
+    const stringified = fs.readFileSync(path.join(process.cwd(), 'server','assets', 'cost_of_living_cp.json'), 'utf8');
+    console.log("vercel docs solution:\n",stringified);
 
     // inside process.cwd() we can access server fs
     res.setHeader("Access-Control-Allow-Origin", "*");
